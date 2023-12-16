1373fbd2a4646cd4e153c5554e31d4118747b99a
ChiragSinghal2001 <chirag.singhal5088@gmail.com>
2023-12-16 22:42:15 +0530

    Thirdday

--- javascript/testday2.js
+++ javascript/testday2.js
@@ -45,19 +45,56 @@ const { log } = require("console");
 //  console.log(myarr.splice(3,0,4));
 //  console.log(myarr);
 
-let newsym =Symbol("kieie");
+// let newsym =Symbol("kieie");
 
-const ps={
-    name:"Chirag",
-    email: "Chirag@happy.com",
-    [newsym]: "keie"
+// const ps={
+//     name:"Chirag",
+//     email: "Chirag@happy.com",
+//     [newsym]: "keie"
 
-}
-console.log(ps);
-console.log(ps["email"]);
-console.log(ps.email);
-console.log(ps[newsym]);
+// }
+// console.log(ps);
+// console.log(ps["email"]);
+// console.log(ps.email);
+// console.log(ps[newsym]);
 
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
 
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
+// console.log(1 === "1");
+ 
 
+// const marvel_heros = ["thor", "Ironman", "spiderman"]
+// const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
