141521e83059a79ea2dc6f03221ef0a9206a3752
ChiragSinghal2001 <chirag.singhal5088@gmail.com>
2023-12-21 23:55:44 +0530

    6thday

---
+++ javascript/testday5.js
@@ -0,0 +1,285 @@
+// // const tinderUser = new Object()
+// const tinderUser = {}
+
+// tinderUser.id = "123abc"
+// tinderUser.name = "Sammy"
+// tinderUser.isLoggedIn = false
+
+// // console.log(tinderUser);
+
+// const regularUser = {
+//     email: "some@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "hitesh",
+//             lastname: "choudhary"
+//         }
+//     }
+// }
+
+// // console.log(regularUser.fullname.userfullname.firstname);
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
+
+// // const obj3 = { obj1, obj2 }
+// // const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// // const obj3 = {...obj1, ...obj2}
+// // console.log(obj3);
+
+
+// const users = [
+//     {
+//         id: 1,
+//         email: "h1@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h2@gmail.com"
+//     },
+//     {
+//         id: 1,
+//         email: "h3@gmail.com"
+//     },
+// ]
+
+// // users[1].email
+// // console.log(users[1].email);
+// // console.log(tinderUser);
+
+// // console.log(Object.keys(tinderUser));
+// // console.log(Object.values(tinderUser));
+// // console.log(Object.entries(tinderUser));
+
+// // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+// const course = {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
+
+// // course.courseInstructor
+
+// const {courseInstructor: instructor} = course
+
+// console.log(course.courseInstructor);
+// console.log(instructor);
+
+// // {
+// //     "name": "hitesh",
+// //     "coursename": "js in hindi",
+// //     "price": "free"
+// // }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+// function addTwoNumbers(number1, number2){
+
+//     // let result = number1 + number2
+//     // return result
+//     return number1 + number2
+// }
+
+// const result = addTwoNumbers(3, 5)
+
+// // console.log("Result: ", result);
+
+
+// function loginUserMessage(username = "sam"){
+//     if(!username){
+//         console.log("PLease enter a username");
+//         return
+//     }
+//     return `${username} just logged in`
+// }
+
+// // console.log(loginUserMessage("hitesh"))
+// // console.log(loginUserMessage("hitesh"))
+
+
+// function calculateCartPrice(val1, val2, ...num1){
+//     return num1
+// }
+
+// // console.log(calculateCartPrice(200, 400, 500, 2000))
+
+// const user = {
+//     username: "hitesh",
+//     prices: 199
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+
+// // handleObject(user)
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+// const myNewArray = [200, 400, 100, 600];
+
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+
+// console.log(returnSecondValue(myNewArray));
+// console.log(returnSecondValue([200, 400, 500, 1000]));
+
+var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+// console.log(addone(5))
+
+// function addone(num){
+//     return num + 1
+// }
+
+
+// addTwo(5)
+// const addTwo = function(num){
+//     return num + 2
+// }
+
+
+
+
+// const user = {
+//     username: "hitesh",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
+
+// }
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+// console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
+
+
+
+// Immediately Invoked Function Expressions (IIFE)
+
+
+// (function chai(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
+
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('hitesh')
+
+
